85e67a3c00e76ab7f6dd5ce625a00ceac0c62014
Andrew <ajacoboc@unsa.edu.pe>
2022-06-02 02:49:12 -0500

    Chat terminado

--- index.js
+++ index.js
@@ -1,15 +1,60 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+async function emitSocketsActivos() {
+  const sockets = await io.fetchSockets();
+  const chatAactivos = [];
+  for (const socket of sockets) {
+    chatAactivos.push(socket.username);
+  }
+
+  io.emit("users active", { actives: chatAactivos });
+}
+
+io.on("connection", (socket) => {
+  emitSocketsActivos();
+
+  // CUANDO SE CONECTE
+  socket.on("connect", () => {});
+
+  // CUANDO SE DESCONECTA
+  socket.on("disconnect", (reason) => {
+    // actualizamos activos
+    emitSocketsActivos();
+    io.except(socket.id).emit("alert disconnect", {
+      msg: "se desconecto!",
+      user: socket.username,
+    });
+  });
+
+  // CUANDO SE ENVIE UN MENSAJE
+  socket.on("chat message", (anotherSocketId, msg) => {
+    io.except(anotherSocketId).emit("chat message", {
+      user: socket.username,
+      msg,
+    });
+  });
+
+  // CUANDO ESCRIBA
+  socket.on("statusPress", (anotherSocketId, msg) => {
+    io.except(anotherSocketId).emit("statusPress", socket.username + msg);
+  });
+
+  // CUANDO DEJE DE ESCRIBIR
+  socket.on("statusUp", (anotherSocketId, msg) => {
+    io.except(anotherSocketId).emit("statusUp", msg);
+  });
+
+  // AÑADIR USERNAME
+  socket.on("set username", (username) => {
+    socket.username = username;
+    emitSocketsActivos();
   });
 });
 
